977ed5c232c3f2f37b6d234918caf2e0829c72c1
xiaofeiqiu <xqiu35@gmail.com>
2023-12-29 19:04:24 -0800

    b5

--- playground/main.go
+++ playground/main.go
@@ -2,28 +2,18 @@ package main
 
 import "fmt"
 
+type T struct {
+	A int
+}
+
 func main() {
-	nums := []int{1, 2, 3, 4, 5}
-	l := 2
-	h := l
-	k := 3
-	sum := 0
+	a := make([]int, 1)
 
-	count := 0
-	for i := range nums {
-		if h > 0 {
-			sum += nums[i]
-			h--
-			continue
-		}
+	f(append(a, 10))
 
-		if sum == k {
-			count++
-		}
+	fmt.Println(a)
+}
 
-		sum += (nums[i] - nums[i-l])
-		fmt.Printf("i=%d, i-l=%d\n", nums[i], nums[i-l])
-	}
-
-	fmt.Println(count)
+func f(a []int) {
+	fmt.Println(a)
 }
